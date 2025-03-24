2d5ac8224c7ec0332e6661ff2fb7aaeb460f9435
Source <abc@def.com>
2025-03-24 10:53:04 +0530

    Added the changes for sub array calculation

---
+++ Challenge-6/challenge6.py
@@ -0,0 +1,16 @@
+from typing import List
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        current_sum = nums[0]
+        max_sum = nums[0]
+
+        for num in nums[1:]:
+            current_sum = max(num, current_sum + num)
+            max_sum = max(max_sum, current_sum)
+
+        return max_sum
+
+
+obj = Solution()
+nums = [-2,1,-3,4,-1,2,1,-5,4]
+print(obj.maxSubArray(nums))
