4e1bec1fc9bf3e1bae0e6ebb4ef8e44110201169
Source <abc@def.com>
2025-03-26 13:57:08 +0530

    Updated the best solution for the serach in roted sorted array

--- Challenge-9/searchinrotatedsortedarray.py
+++ Challenge-9/searchinrotatedsortedarray.py
@@ -1,12 +1,39 @@
 from typing import List
+
+
 class Solution:
+    # my solution
     def search(self, nums: List[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
-        else:
-            return -1
+        # if target in nums:
+        #     return nums.index(target)
+        # else:
+        #     return -1
+
+    # best solution
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = left + (right - left) // 2
+
+            if nums[mid] == target:
+                return mid
+
+
+            if nums[left] <= nums[mid]:
+                if nums[left] <= target < nums[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if nums[mid] < target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
+
 
 obj = Solution()
-nums = [4,5,6,7,0,1,2]
+nums = [4, 5, 6, 7, 0, 1, 2]
 target = 0
-print(obj.search(nums,target))
+print(obj.search(nums, target))
