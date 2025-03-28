d0b3070def9c72cf883c89aff201a58a768d0854
Source <abc@def.com>
2025-03-28 20:10:58 +0530

    changes for identifying the container with most water

---
+++ Challenge11/containerwithmostwater.py
@@ -0,0 +1,21 @@
+from typing import List
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        max_area = 0
+
+        while left < right:
+            width = right - left
+            current_area = min(height[left], height[right]) * width
+            max_area = max(max_area, current_area)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+
+        return max_area
+
+obj = Solution()
+height = [1,8,6,2,5,4,8,3,7]
+print(obj.maxArea(height))
