95912242a217f1a132146344a30d3defbf938c71
nithinmathew225 <66953809+nithinmathew225@users.noreply.github.com>
2025-05-18 13:23:10 +0530

    Change for invert binary

---
+++ 62-invert-binary.py
@@ -0,0 +1,31 @@
+Prerequisites
+Binary Tree
+Data Structures & Algorithms for Beginners
+Depth-First Search
+Data Structures & Algorithms for Beginners
+Video Explanation
+
+
+View on Youtube
+
+1. Breadth First Search
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+        queue = deque([root])
+        while queue:
+            node = queue.popleft()
+            node.left, node.right = node.right, node.left
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        return root
